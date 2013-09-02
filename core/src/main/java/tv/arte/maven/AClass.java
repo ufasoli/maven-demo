1c59aedfbed35dd27c526092cfa60be1154cda25
Ulises Fasoli <ufasoli@gmail.com>
2013-09-02 16:43:02 +0200

    initial commit

---
+++ core/src/main/java/tv/arte/maven/AClass.java
@@ -0,0 +1,14 @@
+package tv.arte.maven;
+
+/**
+ * User: ufasoli
+ * Date: 02/09/13
+ * Time: 14:29
+ * project : formation-maven
+ */
+public class AClass {
+
+    public static void main(String[] args){
+        System.out.println("HELLO World");
+    }
+}
---
+++ mywar2/src/main/java/java/tv/arte/maven/CClass.java
@@ -0,0 +1,33 @@
+package java.tv.arte.maven;
+
+/**
+ * User: ufasoli
+ * Date: 02/09/13
+ * Time: 14:29
+ * project : formation-maven
+ */
+public class CClass {
+
+    public static void main(String[] args){
+        System.out.println("HELLO World");
+    }
+
+
+    public void duplicateCode(){
+
+        int i = 9;
+        innerMethod();
+
+    }
+
+    public void duplicateCode2(){
+        int i = 9;
+        innerMethod();
+
+    }
+
+
+    private void innerMethod(){
+
+    }
+}
